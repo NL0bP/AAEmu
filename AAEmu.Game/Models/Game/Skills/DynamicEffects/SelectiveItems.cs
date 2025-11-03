47084a7a50dc808d20bd89a2345caba7b53e0215
NLObP <38990417+NL0bP@users.noreply.github.com>
2025-11-03 18:29:19 +0300

    [Game] Fixed reading of ItemSelections data - Items such as ID=23862 "Cloaked Illustrious Dagger" are now unpacked correctly. - Modified the `ItemSelections` class to correctly read item data in `skill_dynamic_effects`.

--- AAEmu.Game/Models/Game/Skills/DynamicEffects/SelectiveItems.cs
+++ AAEmu.Game/Models/Game/Skills/DynamicEffects/SelectiveItems.cs
@@ -1,38 +1,31 @@
 ﻿using System.Collections.Generic;
-using System.Text.Json.Serialization;
-using AAEmu.Game.Utils;
+
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 
-namespace AAEmu.Game.Models.Game.Skills
+namespace AAEmu.Game.Models.Game.Skills;
+
+public class ItemSelections
 {
-    public class ItemSelections
-    {
-        [Newtonsoft.Json.JsonConverter(typeof(JSonHexConverterULong))]
-        [JsonProperty("item")]
-        public ulong EncryptedItem { get; set; }
-
-        // TODO: Add item decryption code
-        [Newtonsoft.Json.JsonIgnore]
-        public uint Item { get; set; }
+    [JsonProperty("item")]
+    public uint Item { get; set; }
         
-        [JsonProperty("count")]
-        public int Count { get; set; }
-    }
+    [JsonProperty("count")]
+    public int Count { get; set; }
+}
 
-    public class SelectiveItems
+public class SelectiveItems
+{
+    public string Effect { get; set; }
+    public int Select { get; set; }
+    public int ConsumeItemCount { get; set; }
+    public List<ItemSelections> ItemSelections { get; set; }
+
+    public SelectiveItems(JObject obj)
     {
-        public string Effect { get; set; }
-        public int Select { get; set; }
-        public int ConsumeItemCount { get; set; }
-        public List<ItemSelections> ItemSelections { get; set; }
-
-        public SelectiveItems(JObject obj)
-        {
-            Effect = obj.GetValue("effect")?.ToString() ?? string.Empty;
-            Select = obj.GetValue("select")?.ToObject<int>() ?? 0;
-            ConsumeItemCount = obj.GetValue("consume_item_count")?.ToObject<int>() ?? 0;
-            ItemSelections = obj.GetValue("list")?.ToObject<List<ItemSelections>>() ?? [];
-        }
+        Effect = obj.GetValue("effect")?.ToString() ?? string.Empty;
+        Select = obj.GetValue("select")?.ToObject<int>() ?? 0;
+        ConsumeItemCount = obj.GetValue("consume_item_count")?.ToObject<int>() ?? 0;
+        ItemSelections = obj.GetValue("list")?.ToObject<List<ItemSelections>>() ?? [];
     }
 }
